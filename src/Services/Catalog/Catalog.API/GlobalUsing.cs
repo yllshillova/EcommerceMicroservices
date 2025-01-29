35427a8fa1636aade127ec5c1ab29d3c26d51903
Yll Shillova <yllshillova@gmail.com>
2025-01-29 21:28:33 +0100

    added pagination for getProducts

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -10,3 +10,5 @@ global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Data;
+global using Marten.Pagination;
+ 
