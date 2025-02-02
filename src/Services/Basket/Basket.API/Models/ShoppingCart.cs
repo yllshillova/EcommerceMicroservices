863b4781ce638db53f6ca5f6f2cd97b2a2f7bd18
Yll Shillova <yllshillova@gmail.com>
2025-02-02 01:01:04 +0100

    Added the second microservice and its models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal Total => Items.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+        //for mapping
+        public ShoppingCart()
+        {
+            
+        }
+
+    }
+}
